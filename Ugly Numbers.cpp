6b1e5df143a885ca78c01b5e3401ac415d640e98
Vibhuti0206 <56320137+Vibhuti0206@users.noreply.github.com>
2020-01-20 16:19:28 +0530

    Create Ugly Numbers.cpp

---
+++ Ugly Numbers.cpp
@@ -0,0 +1,52 @@
+# include<bits/stdc++.h> 
+using namespace std; 
+  
+
+long int UglyNo(long int n) 
+{ 
+    long int ugly[n];  
+    long int i2 = 0, i3 = 0, i5 = 0; 
+    long int next_multiple_of_2 = 2; 
+    long int next_multiple_of_3 = 3; 
+    long int next_multiple_of_5 = 5; 
+    long int next_ugly_no; //= 1; 
+  
+    ugly[0] = 1; 
+    for (int i=1; i<n; i++) 
+    { 
+       next_ugly_no = min(next_multiple_of_2, 
+                           min(next_multiple_of_3, 
+                               next_multiple_of_5)); 
+       ugly[i] = next_ugly_no; 
+       if (next_ugly_no == next_multiple_of_2) 
+       { 
+           i2 = i2+1; 
+           next_multiple_of_2 = ugly[i2]*2; 
+       } 
+       if (next_ugly_no == next_multiple_of_3) 
+       { 
+           i3 = i3+1; 
+           next_multiple_of_3 = ugly[i3]*3; 
+       } 
+       if (next_ugly_no == next_multiple_of_5) 
+       { 
+           i5 = i5+1; 
+           next_multiple_of_5 = ugly[i5]*5; 
+       } 
+    } 
+    return next_ugly_no; 
+} 
+  
+
+int main() 
+{ 
+   long int t;
+   cin>>t;
+   while(t--)
+   {
+       long int n;
+       cin>>n;
+       cout<<UglyNo(n)<<endl;
+   }
+   return 0;
+}
